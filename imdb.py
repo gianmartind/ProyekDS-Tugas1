636c24fa2de7a74c7adab4a7086c90cb6df4d211
gianmartind <gianmartind@gmail.com>
2021-03-08 18:55:46 +0700

    initial

---
+++ imdb.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 12:34:11 2021
+
+@author: gianm
+"""
+
+import pandas as pd
+import matplotlib as plt
+
+data = pd.read_csv("IMDB-Movie-Data.csv")
