79a64ca95b85d2e21ce2e1912aa05e762bf24608
gianmartind <gianmartind@gmail.com>
2021-03-12 06:51:33 +0700

    12 maret

--- imdb.py
+++ imdb.py
@@ -5,7 +5,33 @@ Created on Mon Mar  8 12:34:11 2021
 @author: gianm
 """
 
+#One hot encoding
+
 import pandas as pd
 import matplotlib as plt
 
-data = pd.read_csv("IMDB-Movie-Data.csv")
+data = pd.read_csv("E:\Kuliah\Proyek Data Science\ProyekDS-Tugas1\IMDB-Movie-Data.csv")
+
+allGenre = set()
+
+i = 0
+while i < len(data.Genre):
+    genreArray = data.Genre[i].split(",")
+    for n in genreArray:
+        allGenre.add(n)
+    i = i + 1
+
+
+zeros = [0] * len(data.Genre)
+for n in allGenre:
+    data['{}'.format(n)] = zeros
+
+j = 0
+while j < len(data.Genre):
+    genre = data.Genre[j]
+    for n in allGenre:
+        if genre.find(n) != -1:
+            data['{}'.format(n)][j] = 1
+        else:
+            data['{}'.format(n)][j] = 0
+    j += 1
